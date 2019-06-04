658873cf9a754d72c783fe54ba08db1bfeb93753
DESKTOP-56RLMSB\Roy <rvandepol@live.nl>
2019-06-05 00:32:44 +0200

    Deleted backgroundService

--- app/src/main/java/com/example/roy/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/roy/recipeapp/MainActivity.java
@@ -63,14 +63,9 @@ public class MainActivity extends AppCompatActivity {
         recipeList = new ArrayList<>();
 
         requestData();
-
-
+        
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-
-
 
     }
 
